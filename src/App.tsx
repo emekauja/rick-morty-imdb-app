1c975c08c4b91c762202c19796bbc5586d0ab29a
emekauja <60346432+emekauja@users.noreply.github.com>
2020-09-05 13:08:11 +0100

    'data embedded'

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Store } from './Store'
+
+
+export default function App():JSX.Element {
+  const {state, dispatch} = React.useContext(Store)
+
+  React.useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction()
+  })
+  const fetchDataAction = async () => {
+    const URL = 'https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes'
+    const data = await fetch(URL)
+    const dataJSON = await data.json()
+    return dispatch({
+      type: 'FECTH_DATA',
+      payload: dataJSON._embedded.episodes
+    })
+  }
+  console.log(state)
+   return (
+    <React.Fragment>
+      <h1>Ricky and Morty</h1>
+      <p>Pick your favourite episode!!!</p>
+    </React.Fragment>
+  )
+}
+
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {StoreProvider} from './Store'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StoreProvider>
+      <App />
+  </StoreProvider>,
   document.getElementById('root')
 );
 
